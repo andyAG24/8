cc2c809447ec767cc0dd9b49aafff9631c09e3ac
andyAG24 <andy.ag.24@gmail.com>
2018-11-25 22:18:24 +0300

    Add files via upload

---
+++ exceptions_task_3.py
@@ -0,0 +1,26 @@
+documents = [
+  {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+  {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+  {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
+  {"type": "license", "number": "1236", "name": ""}
+]
+
+directories = {
+  '1': ['2207 876234', '11-2'],
+  '2': ['10006'],
+  '3': []
+}
+
+def show_all_list():
+  print("----------Команда-l(list)------------")
+  i = 0
+  while i <= len(documents) - 1:
+    try:
+        documents[i]["name"] == ""
+    except:
+        documents[i]["name"] == "Не найден владелец"
+    print(documents[i]["type"], documents[i]["number"], documents[i]["name"])
+    i += 1
+  print("\n")
+
+show_all_list()
